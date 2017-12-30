0deab3fad10f9ed620e8e436aad3daa34a19271d
Yuki-14544869 <2015061423@hrbeu.edu.cn>
2017-12-30 14:24:19 +0800

    add git ignore .idea

---
+++ cmake-build-debug/CMakeFiles/DataStructure_In_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructure_In_C.dir/main.c.o"
+  "CMakeFiles/DataStructure_In_C.dir/Chapter1/Triplet/Triplet.c.o"
+  "DataStructure_In_C.pdb"
+  "DataStructure_In_C.exe"
+  "libDataStructure_In_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DataStructure_In_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
